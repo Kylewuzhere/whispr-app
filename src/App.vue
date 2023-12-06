3875db13b1aef975335cfa1807cccfa27ac7604a
kylewuzhere <kyvasquez1026@gmail.com>
2023-12-06 23:35:21 +1300

    style fix

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="bg-green-200 min-h-screen">
+  <div class="bg-neutral-900 min-h-screen">
     <header class=" top-1 h-12 w-full">
       <div>
-        <nav>
+        <nav class="text-white">
           <RouterLink to="/">Home</RouterLink>
           <RouterLink to="/about">About</RouterLink>
           <RouterLink to="/sign-in">Sign In</RouterLink>
--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="flex flex-col justify-center items-center bg-slate-900 text-white mt-20 p-4 border rounded-xl h-[20rem] w-[25rem]">
+  <div class="flex flex-col justify-center items-center bg-neutral-800 text-white mt-20 p-4 rounded-xl h-[20rem] w-[25rem] shadow-[0_7px_30px_0px_rgba(255,255,255,0.3)]
+  ">
     <form class="">
       <div>
         <h1 class="text-4xl mb-4 text-center font-black font-dancing">Whispr</h1>
+
       </div>
-      <button class="bg-slate-600 h-12 rounded-xl px-4">Sign in with Google <font-awesome-icon :icon="['fab', 'google']" /></button>
+      <button class="bg-neutral-600 h-12 rounded-xl px-4">Sign in with Google <font-awesome-icon :icon="['fab', 'google']" /></button>
     </form>
   </div>
 </template>
