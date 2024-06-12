f90a7e46be5c929ea7ab93c72e15b1c8dc97ef33
kylewuzhere <kyvasquez1026@gmail.com>
2024-06-13 10:43:51 +1200

    add: implement authenticator with auth0

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createAuth0 } from '@auth0/auth0-vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faGoogle } from '@fortawesome/free-brands-svg-icons'
@@ -12,6 +13,16 @@ import router from './router'
 library.add(faGoogle)
 
 const app = createApp(App)
+
+app.use(
+  createAuth0({
+    domain: import.meta.env.VITE_AUTH0_DOMAIN,
+    clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+    authorizationParams: {
+      redirect_uri: window.location.origin
+    }
+  })
+)
 
 app.use(createPinia())
 app.use(router)
