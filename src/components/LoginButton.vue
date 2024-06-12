5547bb4dbbe52e870f40503ecc74dae948b24cb3
kylewuzhere <kyvasquez1026@gmail.com>
2024-06-13 10:43:28 +1200

    add: implement authenticator with auth0

---
+++ src/components/LoginButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button class="text-gray-200 bg-neutral-600 h-14 rounded-xl px-4 hover:bg-neutral-700 hover:text-white active:scale-95 transition" @click="login">Log in with Auth0</button>
+  </div>
+</template>
+<script lang="ts">
+  import { useAuth0 } from '@auth0/auth0-vue';
+
+  export default {
+    setup() {
+      const { loginWithRedirect } = useAuth0();
+
+      return {
+        login: () => {
+          loginWithRedirect();
+        } 
+      };
+    }
+  };
+</script>
---
+++ src/components/LogoutButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <button class="dark:text-green-500 hover:text-green-500 dark:hover:text-green-400 transition" @click="logout">Log out</button>
+  </div>
+</template>
+<script lang="ts">
+  import { useAuth0 } from '@auth0/auth0-vue';
+
+  export default {
+    setup() {
+      const { logout } = useAuth0();
+
+      return {
+        logout: () => {
+          logout({ logoutParams: { returnTo: window.location.origin } });
+        }
+      };
+    }
+  };
+</script>
